c8b3d21f62fb53bbe38ba2c46895c89e29dea249
oohose <peytongsnipes@gmail.com>
2025-06-25 01:09:37 -0500

    saved admin/page.ts

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,6 +3,7 @@
 import { Suspense } from 'react'
 import AdminDashboard from '@/components/admin/AdminDashboard'
 
+
 export default function AdminPage() {
   return (
     <Suspense fallback={<div className="text-white">Loading...</div>}>
